c1bcfc782fc6b1630c9f9963a3de486b8da2770b
PrideMuchi <pmuchirawetu@gmail.com>
2025-02-27 14:08:06 +0200

    adding Kundi text

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ app.use(express.json()); // Middleware to parse JSON requests
 app.get("/", (req, res) => {
   res.send("Welcome to the REST API with Kundi!");
 });
+app.get("/test", (req, res) => {
+  res.send(<h3>Test</h3>);
+});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
